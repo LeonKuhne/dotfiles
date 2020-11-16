7ec5a78c4ed764ef713733567b28be218642ccb0
Leon Kuhne <lxk1170@rit.edu>
2020-11-16 13:03:35 -0500

    saving

--- .config/fish/conf.d/sway.fish
+++ .config/fish/conf.d/sway.fish
@@ -26,10 +26,20 @@ function todo
   /home/id/Projects/todo/todo.sh $argv
 end
 
+set -Ux STYLES "/home/id/Projects/reStyles"
+set -Ux EPIPHANY_SCRIPT "/home/id/.local/share/epiphany/user-javascript.js"
 function styles
-  cd /home/id/Projects/restyles
+  cd $STYLES
   vim styles.json
-  geet "autosaving (edits made)"
+  
+  # combine stylesheets
+  cp script.js $EPIPHANY_SCRIPT
+  set styleData (jq -c "." styles.json)
+  sed -i "s/const localData = {}/const localData = $styleData/" $EPIPHANY_SCRIPT
+  echo "Saving..."
+  git add styles.json
+  git commit -m "autosaving styles" -q
+  git push -q
   cd -
 end
 
--- .local/share/epiphany/user-javascript.js
+++ .local/share/epiphany/user-javascript.js
@@ -1,21 +1,37 @@
-window.onload = () => {
-  // fetch the styles
-  fetch("https://lxk1170.github.io/reStyles/styles.json").then(res => res.json()).then(data => {
+// use the local stylesheet (vs the online one)
+// local is faster but requires setup
+const localData = {"www.examcompass.com":{"body":{"background":"black","color":"red"}},"leonkuhne.com":{"body":{"background":"black","color":"magenta"},"img":{"box-shadow":"magenta 0 0 10px"},"#headshot":{"border":"cyan solid 10px"}}}
 
-    let host = window.location.host
-    if (host in data) {
-      let styles = data[host]
-      Object.keys(styles).forEach(selector => {
-        let attr = styles[selector]
-        Object.keys(attr).forEach(style => {
+function decorate(styles) {
+  Object.keys(styles).forEach(selector => {
+    let attr = styles[selector]
+    Object.keys(attr).forEach(style => {
 
-          // apply attributes
-          document.querySelectorAll(selector).forEach(node => {
-            node.style[style] = attr[style]
-          })
-
-        })
+      // apply attributes
+      document.querySelectorAll(selector).forEach(node => {
+        node.style[style] = attr[style]
       })
-    }
+    })
   })
+}
+
+function styleHost(hosts) {
+  let host = window.location.host
+  if (host in hosts) {
+    let styles = hosts[host]
+    decorate(styles)
+  }
+}
+
+window.onload = () => {
+  if (Object.keys(localData).length) {
+    console.log("using local")
+    styleHost(localData)
+  } else {
+    console.log("using public")
+    // fetch the styles
+    fetch("https://lxk1170.github.io/reStyles/styles.json").then(res => res.json()).then(data => {
+      styleHost(data)
+    })
+  }
 }
